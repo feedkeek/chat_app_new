39f4413e578b71bbef9955754c12cc4f5bdabe6f
Jafar Hasanov <feedkeek@gmail.com>
2021-08-04 20:17:36 +0400

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory());
   runApp(ChatApp());
 }
 
